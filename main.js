9e5ae8ef3f8a08679f86db5da636e4759040f762
mas0110 <masoudmohammed13@gmail.com>
2024-01-16 18:31:28 +0100

    gioco difficoltà 1 (100 caselle)

--- main.js
+++ main.js
@@ -2,13 +2,24 @@ let grigliahtml = document.querySelector(".griglia");
 
 document.getElementById("play").addEventListener("click", function() {
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 1; i <= 100; i++) {
 
         let box = document.createElement("div");
 
         box.classList.add("box");
 
         grigliahtml.appendChild(box);
+
+        box.innerText = i 
+
+        box.addEventListener("click", function(){
+
+            this.classList.toggle("active")
+
+            console.log(i)
+
+        }
+        )
     }
 
 }
