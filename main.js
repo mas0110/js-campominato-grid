fca050af45b08179e3cd2f0636596e2cebc36bff
mas0110 <masoudmohammed13@gmail.com>
2024-01-17 11:39:31 +0100

    correzioni e bonus

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 let grigliahtml = document.querySelector(".griglia");
+ 
+let selectLevel = document.getElementById("select")
 
 document.getElementById("play").addEventListener("click", function() {
 
-    for (let i = 1; i <= 100; i++) {
+    grigliahtml.innerHTML = ""
+
+    for (let i = 1; i <= select.value; i++) {
 
         let box = document.createElement("div");
+
+        box.style.setProperty(
+            "flex-basis", `calc(100% / ${Math.sqrt(select.value)})`
+        )
 
         box.classList.add("box");
 
         grigliahtml.appendChild(box);
 
-        box.innerText = i 
+        box.innerText = i
 
         box.addEventListener("click", function(){
 
@@ -21,6 +29,7 @@ document.getElementById("play").addEventListener("click", function() {
         }
         )
     }
+
 
 }
 );
